b05f0d60f62936a0c011192f52d85a79bcafd65a
Sam Doshi <sam@metal-fish.co.uk>
2015-03-06 07:55:36 +0000

    don't inherit underline for linum

--- init/init-theme.el
+++ init/init-theme.el
@@ -152,6 +152,7 @@
                       :background init/solarized-base03)
   (set-face-attribute 'linum nil
                       :foreground init/solarized-base01
+                      :underline nil
                       :height 100)
   (init/evil-state-face 'normal  init/solarized-base02 init/solarized-blue)
   (init/evil-state-face 'insert  init/solarized-base2  init/solarized-green)
@@ -179,6 +180,7 @@
                       :background init/solarized-base2)
   (set-face-attribute 'linum nil
                       :foreground init/solarized-base1
+                      :underline nil
                       :height 100)
   (init/evil-state-face 'normal  init/solarized-base2 init/solarized-blue)
   (init/evil-state-face 'insert  init/solarized-base3  init/solarized-green)
