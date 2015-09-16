5529345834a14561a8491610b4b135eb56f1011d
Sam Doshi <sam@metal-fish.co.uk>
2015-09-16 18:35:03 +0100

    speed up emacs start up

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
-(require 'cask)
-(cask-initialize)
+;; https://redd.it/3kqt6e
+(setq-default gc-cons-threshold 100000000)
 
-(require 'ob-tangle)
-(org-babel-load-file
-  (expand-file-name "README.org" user-emacs-directory))
+(let ((file-name-handler-alist nil))
+  (require 'cask)
+  (cask-initialize)
+
+  (require 'ob-tangle)
+  (org-babel-load-file
+   (expand-file-name "README.org" user-emacs-directory)))
